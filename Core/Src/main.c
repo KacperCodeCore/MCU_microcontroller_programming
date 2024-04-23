a78667ff45be26d18f28520dc41ed2f73c094738
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-04-23 11:07:42 +0200

    9 fix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -68,40 +68,8 @@ static void MX_IWDG_Init(void);
 /* USER CODE BEGIN 0 */
 
 int received_number = 0;
-bool led_states[] = { false, false, false, false, false, false};
-bool pressed = false;
-bool enter_pressed = false;
-bool user_button_pressed = false;
 bool lD2_state = true;
-int alarms_time[3][5];
-int alarm_number = 0;
-int number = 0;
-int current_minute = 0;
-int current_secound = 0;
 
-//void led_set_1(int led, bool turn_on)
-//{
-// GPIO_PinState state = (turn_on) ? GPIO_PIN_SET : GPIO_PIN_RESET;
-// switch (led) {
-// case 1:
-// HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, state);
-// break;
-// case 2:
-// HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, state);
-// break;
-// case 3:
-// HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, state);
-// break;
-// case 4:
-// HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, state);
-// break;
-// case 5:
-// HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, state);
-// break;
-// default:
-// break;
-// }
-//}
 
 int __io_putchar(int ch)
 {
@@ -113,25 +81,6 @@ int __io_putchar(int ch)
  return 1;
 }
 
-//bool is_user_button_pressed(int button) {
-//  switch (button) {
-//  case 0:
-//    if (HAL_GPIO_ReadPin(USER_BUTTON3_GPIO_Port, USER_BUTTON3_Pin) == GPIO_PIN_RESET) {
-//      return true;
-//    } else {
-//      return false;
-//    }
-//  case 1:
-//    if (HAL_GPIO_ReadPin(USER_BUTTON2_GPIO_Port, USER_BUTTON2_Pin) == GPIO_PIN_RESET) {
-//      return true;
-//    } else {
-//      return false;
-//    }
-//
-//  default:
-//    return false;
-//  }
-//}
 
 bool is_button_pressed(void) {
  if (HAL_GPIO_ReadPin(USER_BUTTON_GPIO_Port, USER_BUTTON_Pin) == GPIO_PIN_RESET) {
@@ -140,55 +89,6 @@ bool is_button_pressed(void) {
  return false;
  }
 }
-
-
-
-#define LINE_MAX_LENGTH 80
-static char line_buffer[LINE_MAX_LENGTH + 1];
-static uint32_t line_length;
-void line_append(uint8_t value)
-{
- if (value == '\r' || value == '\n') {
- // odebraliśmy znak końca linii
- if (line_length > 0) {
- // jeśli bufor nie jest pusty to dodajemy
- // 0 na końcu linii
- line_buffer[line_length] = '\0';
- enter_pressed = true;
- // przetwarzamy dane
-
- printf("%s", line_buffer);
- received_number = atoi(line_buffer);
-
- // zaczynamy zbieranie danych od nowa
- line_length = 0;
- }
- }
- else {
- if (line_length >= LINE_MAX_LENGTH) {
- // za dużo danych, usuwamy wszystko co
- // odebraliśmy
- line_length = 0;
- }
- // dopisujemy wartość do bufora
- line_buffer[line_length++] = value;
- }
-}
-
-int hours = 0;
-int minutes = 0;
-
-void print_alarms(){
-	  printf("\n");
-	  int i;
-	  for (i = 0; i < 5; i++) {
-		  printf("%01d Alarmu %02d:%02d ->stan:%01d\n", i+1, alarms_time[0][i], alarms_time[1][i], alarms_time[2][i]);
-	  }
-	  printf("\n");
-}
-
-
-
 
 
 volatile uint32_t push_counter;
@@ -251,17 +151,6 @@ int main(void)
 
 
 
-	  const char message[] = "Set alarms time!\r\n";
-	  HAL_UART_Transmit(&huart2, (uint8_t*)message, strlen(message), HAL_MAX_DELAY);
-
-	  	  	  // zerowanie czasu
-	  		   RTC_TimeTypeDef new_time = {0};
-	  		   new_time.Hours = 0;
-	  		   new_time.Minutes = 0;
-	  		   new_time.Seconds = 40;
-	  		   HAL_RTC_SetTime(&hrtc, &new_time, RTC_FORMAT_BIN);
-
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -286,158 +175,9 @@ int main(void)
 		  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 		  last_ms = HAL_GetTick();
 	  }
-//	  if (is_button_pressed()) {
-//		  while(1){}
-//	  }
-
-
-//	  // ustalanie godziny i minut alarmu
-//	  if (is_user_button_pressed(0)) {
-//		  if(user_button_pressed == false){
-//			  user_button_pressed = true;
-//		  	led_set_1(2, true);
-//		  	hours ++;
-//		  	if (hours >= 24){
-//			  	hours = 0;
-//		  	}
-//		  	printf("Czas alarmu: %02d:%02d\n", hours, minutes);
-//		  }
-//	  }
-//	  else if (is_user_button_pressed(1)) {
-//		  if(user_button_pressed == false){
-//			  user_button_pressed = true;
-//			  led_set_1(1, true);
-//			  minutes++;
-//			  if (minutes >= 60){
-//	 		 	minutes = 0;
-//			  }
-//			  printf("Czas alarmu: %02d:%02d\n", hours, minutes);
-//		  }
-//	  }
-//	  else if (is_button_pressed()) {
-//		  if(user_button_pressed == false){
-//			  user_button_pressed = true;
-//			  printf("\n");
-//			  led_set_1(2, true);
-//
-//			  if (alarm_number > 5){
-//				  alarm_number = 1;
-//			  }
-//			  alarms_time[0][alarm_number] = hours;
-//			  alarms_time[1][alarm_number] = minutes;
-//
-//			  print_alarms();
-//			  alarm_number++;
-//		  }
-//	  }
-//	  else{
-//		  user_button_pressed = false;
-//		  led_set_1(1, false);
-//		  led_set_1(2, false);
-//	  }
 
 
 
-
-
-
-//	  // aktualny czas + wyswietlanie
-//	  RTC_TimeTypeDef time;
-//	  RTC_DateTypeDef date;
-//	  //wyświetlanie czasu
-//	   HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
-//	   HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
-////	   if(current_secound != time.Seconds){
-////		   current_secound = time.Seconds;
-////		   printf("Aktualny czas: %02d:%02d:%02d\n", time.Hours, time.Minutes, time.Seconds);
-////	   }
-
-
-//	   // sprawdzanie co minutę
-//	   if(current_minute != time.Minutes){
-//		   current_minute = time.Minutes;
-//		   print_alarms();
-//		   //porównanie alarmu z czasem
-////		   printf("-> checking alarms \n");
-////		   printf("-> alarms_time[0][i] Hours: %d - %d \n", alarms_time[0][i],time.Hours);
-////		   printf("-> alarms_time[1][i] Minutes: %d - %d \n", alarms_time[1][i],time.Minutes);
-//		   int i;
-//		   for (i = 0; i < 5; i++) {
-//
-//			  if(alarms_time[0][i] == time.Hours && alarms_time[1][i] == time.Minutes){
-//				  printf("\n");
-//
-//				  if(alarms_time[2][i] == 1)
-//				  {
-//					  led_set_1(3, true);
-//					  printf("-> Alarm: %d, dioda: on\n", i+1);
-//				  }
-//				  else{
-//					  led_set_1(3, false);
-//					  printf("!!! Alarm: %d, dioda: off\n", i+1);
-//				  }
-//			  }
-//		   }
-//		   printf("\n");
-//	   }
-
-
-
-
-
-
-
-
-	  uint8_t value;
-	  if (HAL_UART_Receive(&huart2, &value, 1, 0) == HAL_OK)
-	  {
-	      line_append(value);
-	  }
-
-//	  if(enter_pressed){
-//		  enter_pressed = false;
-////	      printf("\r dioda LED ");
-////	      if(led_states[received_number]){
-////	    	  printf("off ");
-////	      }else{
-////	    	  printf("on ");
-////	      }
-////	      printf("%d\n",received_number);
-//		  received_number = received_number - 1;
-//
-//	      if(alarms_time[2][received_number] == 0){
-//	    	  alarms_time[2][received_number] = 1;
-//	      }else{
-//	    	  alarms_time[2][received_number] = 0;
-//	      }
-//
-//	      print_alarms();
-//
-//	      led_states[received_number] = !led_states[received_number];
-//	      led_set_1(received_number, led_states[received_number]);
-//	  }
-
-
-//	  // jeśli naciśnięto przycisk
-//	   if (is_button_pressed()) {
-//		   number++;
-//		   led_set_1(number, true);
-//
-////		   // ważne: nieużywane pola muszą być wyzerowane
-////		   RTC_TimeTypeDef new_time = {0};
-////		   // czekamy na zwolnienie przycisku
-////		   while (is_button_pressed()) {}
-////		   // ustawiamy godzinę 07:45:00
-////		   new_time.Hours = 7;
-////		   new_time.Minutes = 45;
-////		   new_time.Seconds = 0;
-////		   HAL_RTC_SetTime(&hrtc, &new_time, RTC_FORMAT_BIN);
-//	   }else{
-//		   led_set_1(number, false);
-//		   if(number >= 5){
-//			   number = 0;
-//		   }
-//	   }
 
 
     /* USER CODE END WHILE */
